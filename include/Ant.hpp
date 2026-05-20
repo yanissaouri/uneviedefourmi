3dace015bdcc11d85e2ebe895712158f2a2b0baf
achraf_nait_belkacem <achrafoonb@gmail.com>
2026-05-20 15:04:21 +0200

    update

--- include/Ant.hpp
+++ include/Ant.hpp
@@ -1,12 +1,24 @@
 #pragma once
 
+#include <cstdint>
+#include <string>
+
 class Ant
 {
     public:
         Ant();
         ~Ant();
 
+        uint16_t    getId(void);
+        std::string getName(void);
+
+        void        setId(uint16_t id);
+        void        setName(std::string name);
+        Room        *getRoomPtr(void);
+
     private:
-        bool    _isWaiting;
-        
+        bool _isWaiting;
+        uint16_t    _id;
+        std::string _name;    
+        Room *_roomPtr;  
 };
--- src/main.cpp
+++ src/main.cpp
@@ -9,23 +9,23 @@ int main(void)
 
     Room room[14];
 
-    room[0].setRoomSize(8);
-    room[1].setRoomSize(4);
-    room[2].setRoomSize(2);
-    room[3].setRoomSize(4);
-    room[4].setRoomSize(2);
-    room[5].setRoomSize(4);
-    room[6].setRoomSize(2);
-    room[7].setRoomSize(5);
-    room[8].setRoomSize(0);
-    room[9].setRoomSize(0);
-    room[10].setRoomSize(0);
-    room[11].setRoomSize(0);
-    room[12].setRoomSize(4);
-    room[13].setRoomSize(2);
+    // room[0].setRoomSize(8);
+    // room[1].setRoomSize(4);
+    // room[2].setRoomSize(2);
+    // room[3].setRoomSize(4);
+    // room[4].setRoomSize(2);
+    // room[5].setRoomSize(4);
+    // room[6].setRoomSize(2);
+    // room[7].setRoomSize(5);
+    // room[8].setRoomSize(0);
+    // room[9].setRoomSize(0);
+    // room[10].setRoomSize(0);
+    // room[11].setRoomSize(0);
+    // room[12].setRoomSize(4);
+    // room[13].setRoomSize(2);
 
-    room[0].setNextRoomPtr(&room[1]);
-    room[1].setNextRoomPtr(&room[2]);
+    // room[0].setNextRoomPtr(&room[1]);
+    // room[1].setNextRoomPtr(&room[2]);
 
     return 0;
 }
